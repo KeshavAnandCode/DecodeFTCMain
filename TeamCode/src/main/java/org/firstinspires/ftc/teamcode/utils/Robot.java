11219015ac2efebedb18e33e90f7921e61a2a784
KeshavAnandCode <keshavanandofficial@gmail.com>
2025-09-07 18:50:44 -0500

    Boilerplate for Robot.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Robot {
+
+    //Initialize Public Components
+
+    public Robot (HardwareMap hardwareMap) {
+
+        //Define components w/ hardware map
+
+    }
+}
