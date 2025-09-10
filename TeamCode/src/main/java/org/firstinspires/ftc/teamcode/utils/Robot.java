497de042cd599b23800e9b4885dd8e63d4f1c237
sujayGG <sujaygonchigar@gmail.com>
2025-09-09 20:37:05 -0500

    Initialized AprilTagDetection.java and added webcam to Robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
@@ -4,12 +4,18 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
 public class Robot {
 
     //Initialize Public Components
 
     public DcMotorEx flywheel1;
     public DcMotorEx flywheel2;
+    public WebcamName visionProccesorAprilTag;
+    public AprilTagProcessor aprilTag;
 
     public Robot (HardwareMap hardwareMap) {
 
@@ -18,6 +24,8 @@ public class Robot {
 
         flywheel1 = hardwareMap.get(DcMotorEx.class, "fly1"); //Port 0
         flywheel2 = hardwareMap.get(DcMotorEx.class, "fly2"); //Port 0
+        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
+        visionProccesorAprilTag = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), aprilTag).getActiveCamera();
 
         //Motor Direction
 
