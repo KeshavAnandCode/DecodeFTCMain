36fd21b5f3e50b82d35d0c45c6e424d79fadc0b9
DanTheMan-byte <danibarrf@gmail.com>
2025-10-02 19:35:50 -0500

    Color Sensors added to Robot - 10/2/2025

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
@@ -15,7 +15,12 @@ public class Robot {
     public DcMotorEx flywheel1;
     public DcMotorEx flywheel2;
     public Limelight3A limelight;
-    public DigitalChannel Pin0;
+    public DigitalChannel color1_0;
+    public DigitalChannel color1_1;
+    public DigitalChannel color2_0;
+    public DigitalChannel color2_1;
+    public DigitalChannel color3_0;
+    public DigitalChannel color3_1;
     public Robot (HardwareMap hardwareMap) {
 
         //Define components w/ hardware map
@@ -25,6 +30,14 @@ public class Robot {
 
         flywheel1.setDirection(DcMotorSimple.Direction.FORWARD);
         flywheel2.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        color1_0 = hardwareMap.get(DigitalChannel.class,"color10"); //Port0-1
+        color1_1 = hardwareMap.get(DigitalChannel.class,"color11"); //Port0-1
+        color2_0 = hardwareMap.get(DigitalChannel.class,"color20"); //Port2-3
+        color2_1 = hardwareMap.get(DigitalChannel.class,"color21"); //Port2-3
+        color3_0 = hardwareMap.get(DigitalChannel.class,"color30"); //Port4-5
+        color3_1 = hardwareMap.get(DigitalChannel.class,"color31"); //Port4-5
+
 
         if (USING_LL) {
             limelight = hardwareMap.get(Limelight3A.class, "limelight");
