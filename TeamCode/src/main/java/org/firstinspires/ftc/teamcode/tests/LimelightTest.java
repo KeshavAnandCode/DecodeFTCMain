aea3f0b2d4fcc66bfa72859d9bb253bfe87d6781
KeshavAnandCode <keshavanandofficial@gmail.com>
2025-10-11 21:45:11 -0500

    more updates

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LimelightTest.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.utils.Robot;
+import org.firstinspires.ftc.teamcode.utils.subsystems.Limelight;
+
+
+@Autonomous
+@Config
+
+public class LimelightTest extends LinearOpMode {
+
+    public static String MODE = "AT";
+    public static int pipe = 1;
+
+
+    MultipleTelemetry TELE;
+
+    Robot robot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        robot = new Robot(hardwareMap);
+
+        TELE = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        Limelight limelight = new Limelight(robot, TELE);
+
+        limelight.setMode(MODE);
+
+        limelight.setTelemetryOn(true);
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        while(opModeIsActive()){
+
+            limelight.setPipeline(pipe);
+
+            limelight.setMode(MODE);
+
+            limelight.update();
+
+            TELE.update();
+
+        }
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Vars.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class Vars {
+
+    public static boolean USING_LL = false;
+}
