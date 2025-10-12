065bcfa40bb9f525c412af96765e39d2b9beec8c
KeshavAnandCode <keshavanandofficial@gmail.com>
2025-10-11 20:30:55 -0500

    Color Sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/ColorSensorTester.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+@Config
+@TeleOp
+public class ColorSensorTester extends LinearOpMode {
+
+
+    public static String portAName = "pin0";
+
+    public static String portBName = "pin1";
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        DigitalChannel pinA = hardwareMap.digitalChannel.get(portAName);
+        DigitalChannel pinB = hardwareMap.digitalChannel.get(portBName);
+
+
+
+
+        MultipleTelemetry TELE = new MultipleTelemetry(
+                telemetry,
+                FtcDashboard.getInstance().getTelemetry()
+        );
+
+
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        while(opModeIsActive()){
+
+            TELE.addData("pinA", pinA.getState());
+            TELE.addData("pinB", pinB.getState());
+
+            TELE.update();
+
+
+
+
+        }
+
+
+
+    }
+}
