8d00c4dd0f2b00dc953f176517ed1a684165c5c4
KeshavAnandCode <keshavanandofficial@gmail.com>
2025-11-01 16:56:14 -0500

    final commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -2,12 +2,20 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.rowanmcalpin.nextftc.core.Subsystem;
 
 import org.firstinspires.ftc.teamcode.utils.Robot;
 
 public class Intake extends Subsystem {
+
+    private GamepadEx gamepad;
+
+    public MultipleTelemetry TELE;
+
+
+    private DcMotorEx intake;
 
 
     public Intake (Robot robot, MultipleTelemetry telemetry, GamepadEx gamepad){
