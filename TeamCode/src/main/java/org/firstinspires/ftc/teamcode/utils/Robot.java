6a584fe4cad485efdfc94633d28787b37ff0ff7d
KeshavAnandCode <keshavanandofficial@gmail.com>
2025-10-31 20:35:50 -0500

    Added drivetrain motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
@@ -1,14 +1,34 @@
 package org.firstinspires.ftc.teamcode.utils;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorImplEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Robot {
 
     //Initialize Public Components
 
+    public DcMotorEx frontLeft;
+    public DcMotorEx frontRight;
+
+    public DcMotorEx backLeft;
+
+    public DcMotorEx backRight;
+
+
+
+
     public Robot (HardwareMap hardwareMap) {
 
         //Define components w/ hardware map
+
+        frontLeft = hardwareMap.get(DcMotorEx.class, "fl");
+        frontRight = hardwareMap.get(DcMotorEx.class, "fr");
+        backLeft = hardwareMap.get(DcMotorEx.class, "bl");
+        backRight = hardwareMap.get(DcMotorEx.class, "br");
+
+
+
 
     }
 }
