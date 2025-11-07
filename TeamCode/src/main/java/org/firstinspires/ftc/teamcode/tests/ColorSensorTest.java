bb821d9108c194963dddfd73fa3a91faf8bbe932
DanTheMan-byte <danibarrf@gmail.com>
2025-11-06 19:21:56 -0600

    color test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ColorSensorTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.utils.Robot;
+
+@TeleOp
+@Config
+public class ColorSensorTest extends LinearOpMode{
+    Robot robot;
+    MultipleTelemetry TELE;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        robot = new Robot(hardwareMap);
+        TELE = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()){
+            TELE.addData("Green1:", robot.pin0.getState());
+            TELE.addData("Purple1:", robot.pin1.getState());
+            TELE.addData("Green2:", robot.pin2.getState());
+            TELE.addData("Purple2:", robot.pin3.getState());
+            TELE.addData("Green3:", robot.pin4.getState());
+            TELE.addData("Purple3:", robot.pin5.getState());
+
+            TELE.update();
+        }
+    }
+
+}
