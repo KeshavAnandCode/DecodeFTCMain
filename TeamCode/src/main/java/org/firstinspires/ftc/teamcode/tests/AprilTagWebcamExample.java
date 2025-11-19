c7f9028011e1afb8c1187ad04494ecb87b89db92
KeshavAnandCode <keshavanandofficial@gmail.com>
2025-11-18 20:22:55 -0600

    Added April Tag Class and Working Example

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/AprilTagWebcamExample.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.AprilTagWebcam;
+
+
+@Config
+@TeleOp
+public class AprilTagWebcamExample extends OpMode {
+
+    MultipleTelemetry TELE;
+    AprilTagWebcam aprilTagWebcam = new AprilTagWebcam();
+
+    @Override
+    public void init() {
+        TELE = new MultipleTelemetry(
+                telemetry, FtcDashboard.getInstance().getTelemetry()
+        );
+
+        aprilTagWebcam.init(hardwareMap, TELE);
+
+    }
+
+    @Override
+    public void loop() {
+
+        aprilTagWebcam.update();
+        aprilTagWebcam.displayAllTelemetry();
+        TELE.update();
+
+    }
+}
